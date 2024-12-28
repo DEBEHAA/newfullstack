d08244d13d92940ca4363af68d30671632aa7092
debe <debe@gmail.com>
2024-12-28 11:36:51 +0530

    third commit

---
+++ js/loop.js
@@ -0,0 +1,35 @@
+//for loop
+
+for(let i=0 ;i<10;i++)
+{
+    console.log(i," ")
+}
+ 
+//while loop
+a =0
+while(a<=10)
+{
+    ++a
+    console.log(a," ")
+}
+c=0
+do
+{
+   c++
+   if(c>0)
+   {
+    console.log(c," ")
+   }
+}while(c<=10)
+
+//for in loop
+array = [10,20,30,40,50]
+for(var a in array)
+{
+    console.log(a," ")
+}
+//for of loop
+for(var a of array)
+{
+    console.log(a, " ")
+}
