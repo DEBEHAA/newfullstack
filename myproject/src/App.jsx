06fd8556407b76b276f29cf45a1c7b8d6460e18d
debe <debe@gmail.com>
2025-01-01 12:36:51 +0530

    initial hooks

--- myproject/src/App.jsx
+++ myproject/src/App.jsx
@@ -9,7 +9,10 @@ import Gallery from './components/funcomp/Gallery'
 import Home from './components/funcomp/Home'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 import Footer from './components/funcomp/Footer';
-
+import UseEffect from './components/funcomp/UseEffect'
+import UseRef from './components/funcomp/UseRef'
+import UseContext from './components/funcomp/Usecontext';
+import UseMemo from './components/funcomp/UseMemo';
 function App() {
   return (
     <div>
@@ -20,6 +23,11 @@ function App() {
           <Route path="/about" element= {<About college = "Kongu Engineering" clg1 = "Kongu Arts" clg2 = "Naturopathy"/>} />
           <Route path="/gallery" element = {<Gallery />} />
           <Route path="/contact" element = {<Contact />} />
+          <Route path="/use-effect" element = {<UseEffect/>} />
+          <Route path="/use-ref" element = {<UseRef/>} />
+          <Route path="/use-context" element = {<UseContext/>} />
+          <Route path="/use-memo" element = {<UseMemo/>} />
+
         </Routes>
         <Footer/>
       </BrowserRouter>
